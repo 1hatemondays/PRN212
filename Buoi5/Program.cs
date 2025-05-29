6ce996ab97c9141fe76f77524a9c4931cf07f3bc
ihatemondais <ngoquangkhoi1508@gmail.com>
2025-05-29 10:03:07 +0700

    Initial commit

---
+++ Buoi5/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Text;
+using OOP3_Extension_Method;
+Console.OutputEncoding =  System.Text.Encoding.UTF8;
+
+int n1 = 5;
+Console.WriteLine("Tong tu 1 den 5= " + n1.TongTu1DenN());
+int n2 = 10;
+Console.WriteLine("Tong tu 1 den 10=" + n2.TongTu1DenN());
+Console.WriteLine("Tong tu 1 den 100=" +100.TongTu1DenN());
+
+Console.WriteLine("10+20=" + 10.Sum(20));
+
+int[]arr = new int[8];
+arr.TaoMang();
+Console.WriteLine("Truoc khi sap xep: ");
+arr.XuatMang();
+Console.WriteLine("Mang sau khi sap xep tang dan: ");
+arr.SapXepTangDan();
+arr.XuatMang();
+Console.WriteLine();
---
+++ OOP2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System.Text;
+using OOP2;
+
+Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+FulltimeEmployee fulltimeEmployee = new FulltimeEmployee()
+{
+    Id = 1,
+    IdCard = "123",
+    Name = "Dang Mon",
+    Birthday = new DateTime(1960, 11, 25)
+};
+Console.WriteLine("--Thong tin cua con me may--");
+Console.WriteLine($"Id={fulltimeEmployee.Id}");
+Console.WriteLine("IdCard= " + fulltimeEmployee.IdCard);
+Console.WriteLine("Name= " + fulltimeEmployee.Name);
+Console.WriteLine("Birthday= " + fulltimeEmployee.Birthday.ToString("dd/MM/yyyy"));
+Console.WriteLine("Muc luong: " + fulltimeEmployee.calSalary());
+
+
+PartTimeEmployee partTimeEmployee = new PartTimeEmployee()
+{
+    Id = 2,
+    IdCard = "123",
+    Name = "cu Bill",
+    Birthday = new DateTime(2005, 2, 14)
+};
+
+partTimeEmployee.WorkingHours = 3;
+Console.WriteLine("--Thong tin cua con me may--");
+Console.WriteLine($"Id={partTimeEmployee.Id}");
+Console.WriteLine("IdCard= " + partTimeEmployee.IdCard);
+Console.WriteLine("Name= " + partTimeEmployee.Name);
+Console.WriteLine("Birthday= " + partTimeEmployee.Birthday.ToString("dd/MM/yyyy"));
+Console.WriteLine("Muc luong: " + partTimeEmployee.calSalary());
+
+Console.WriteLine("--------------USE   TOSTRING()-------------");
+Console.WriteLine(fulltimeEmployee);
+Console.WriteLine(partTimeEmployee);
