6f36aca26def31e05268af94ef9e89a898f70238
ihatemondais <ngoquangkhoi1508@gmail.com>
2025-06-28 19:47:54 +0700

    Add WPF

---
+++ ProductManagementDemo/BusinessObjects/Category.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+
+namespace BusinessObjects
+{
+    public partial class Category
+    {
+        public Category()
+        {
+            Products = new HashSet<Product>();
+        }
+
+        public Category(int catID, string catName)
+        {
+            this.CatID = catID;
+            this.CatName = catName;
+        }
+        public int CatID { get; set; }
+        public string CatName { get; set; }
+        public virtual ICollection<Product> Products { get; set; }
+    }
+}
---
+++ ProductManagementDemo/Repositories/ProductRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BusinessObjects;
+using DataAccessLayer;
+
+namespace Repositories
+{
+    public class ProductRepository : IProductRepository
+    {
+        private readonly ProductDAO _productDAO = new ProductDAO();
+
+        public void DeleteProduct(Product product) => _productDAO.DeleteProduct(product);
+        public void SaveProduct(Product product) => _productDAO.SaveProduct(product);
+        public void UpdateProduct(Product product) => _productDAO.UpdateProduct(product);
+        public List<Product> GetProducts() => _productDAO.GetProducts();
+        public Product GetProductById(int id) => _productDAO.GetProductById(id);
+    }
+}
